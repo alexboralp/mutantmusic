1ffca13bc6406bf31d623d4d71162cda9daa4c25
Alexander <aborbon@itcr.ac.cr>
2019-05-18 14:39:28 -0600

    Inicio de los arreglos para el algoritmo genetico

--- dist/esconnection.d.ts
+++
@@ -1,2 +0,0 @@
-declare const elasticsearch: any;
-declare const client: any;
--- dist/mutantmusic.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Filename: mutantmusic.ts
- * Author: aborbon
- * Date: 04/10/2019
- * Description: Manejador de mix de música
- */
-export {};
